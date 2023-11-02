24f4f25c79e44736b9be47010be117298012bd5c
brayanmr24 <brayanmr24@ciencias.unam.mx>
2023-11-02 11:17:03 -0600

    Versión para el caso escalar. Faltan correcciones de presición

--- tareas/Brayan/integracion_taylor.jl
+++ tareas/Brayan/integracion_taylor.jl
@@ -29,11 +29,11 @@ module IntegTaylor
         p₂ = findall(x-> x!=0, u.coeffs)[end-1]
         u₁, u₂ = u.coeffs[p₁], u.coeffs[p₂]
         h = min((ϵ/abs(u₁))^(1/(p₁-1)),(ϵ/abs(u₂))^(1/(p₂-1)))
-        h = h*0.5
+        h = h*0.41
         return h
     end
 
-    function paso_integracion(u::Vector{Taylor1},ϵ)
+    function paso_integracion(u::Vector,ϵ)
         pasos = []
         for x ∈ u
             h = paso_integracion(x,ϵ)
@@ -56,16 +56,16 @@ module IntegTaylor
     end
 
 
-    function integracion_taylor(f,x₀,t₀,tₙ,orden,ϵ,p)
-        t = t₀ + Taylor1(orden)
-        u = x₀ + Taylor1(orden)
+    function integracion_taylor(f,x₀::Number,t₀,tₙ,orden,ϵ,p)
+        t = big(t₀) + Taylor1(orden)
+        u = big(x₀) + Taylor1(orden)
         tiempos = [t₀]
         posiciones = [x₀]
         i = 0
         if(t₀<tₙ)
-            while(i<=100)
-                t = tiempos[end] + Taylor1(orden)
-                u = posiciones[end] + Taylor1(orden)
+            while(i<=500)
+                t = big(tiempos[end]) + Taylor1(orden)
+                u = big(posiciones[end]) + Taylor1(orden)
                 u, h = paso_taylor(f,t,u,p,ϵ)
                 t₁ = tiempos[end]+h
                 x₁ = evaluate(u,h)
@@ -77,9 +77,9 @@ module IntegTaylor
                 end
             end
         else
-            while(i<=100)
-                t = tiempos[end] + Taylor1(orden)
-                u = posiciones[end] + Taylor1(orden)
+            while(i<=500)
+                t = big(tiempos[end]) + Taylor1(orden)
+                u = big(posiciones[end]) + Taylor1(orden)
                 u, h = paso_taylor(f,t,u,p,ϵ)
                 t₁ = tiempos[end]-h
                 x₁ = evaluate(u,-h)
@@ -95,6 +95,69 @@ module IntegTaylor
         pop!(posiciones)
         h_final = tₙ - tiempos[end]
         x₁ = evaluate(u,h_final)
+        push!(posiciones, x₁)
+        push!(tiempos, tₙ)
+        return tiempos, posiciones
+    end
+
+    function integracion_taylor(f!,x₀::Vector,t₀,tₙ,orden,ϵ,p)
+        u = []
+        du = []
+        tiempos = [t₀]
+        posiciones = [x₀]
+        i = 0
+        if(t₀<tₙ)
+            while(i<=1000)
+                u = []
+                du = []
+                t = big(tiempos[end]) + Taylor1(orden)
+                for k ∈ posiciones[end]
+                    push!(u, big(k)+Taylor1(orden))
+                    push!(du, Taylor1(orden))
+                end
+                h = paso_taylor!(f!,t,u,du,p,ϵ)
+                t₁ = tiempos[end]+h
+                x₁ = []
+                for k ∈ u
+                    push!(x₁, evaluate(k,h))
+                end
+                push!(tiempos,t₁)
+                push!(posiciones,x₁)
+                i += 1
+                if(t₁>tₙ)
+                    break
+                end
+            end
+        else
+            while(i<=1000)
+                u = []
+                du = []
+                t = big(tiempos[end]) + Taylor1(orden)
+                for k ∈ posiciones[end]
+                    push!(u, big(k)+Taylor1(orden))
+                    push!(du, Taylor1(orden))
+                end
+                h = paso_taylor!(f!,t,u,du,p,ϵ)
+                t₁ = tiempos[end]-h
+                x₁ = []
+                for k ∈ u
+                    push!(x₁, evaluate(k,-h))
+                end
+                push!(tiempos,t₁)
+                push!(posiciones,x₁)
+                i += 1
+                if(t₁<tₙ)
+                    break
+                end
+            end
+        end
+        pop!(tiempos)
+        pop!(posiciones)
+        h_final = tₙ - tiempos[end]
+        x₁ = []
+        for k ∈ u
+            push!(x₁, evaluate(k,h_final))
+        end
         push!(posiciones, x₁)
         push!(tiempos, tₙ)
         return tiempos, posiciones
